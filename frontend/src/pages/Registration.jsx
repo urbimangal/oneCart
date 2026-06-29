f5f14a26fb9be45633f449afe79e01c1474cdd4e
Urbi Mangal <urbimangal@gmail.com>
2026-06-29 16:04:33 +0530

    Fix import case for userDataContext

--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -8,7 +8,7 @@ import { authDataContext } from '../context/AuthContext';
 import axios from "axios"
 import { signInWithPopup } from 'firebase/auth';
 import { auth, provider } from '../../utils/firebase';
-import { userDataContext } from '../context/userContext';
+import { userDataContext } from '../context/UserContext';
 function Registration() {
   let [show,setShow]=useState(false);
   let navigate=useNavigate();
