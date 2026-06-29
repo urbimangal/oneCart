0df41f879de75fa4d37c75e251c4a1ddb85a9e73
Urbi Mangal <urbimangal@gmail.com>
2026-06-29 16:35:56 +0530

    Update CORS origin for production deployment

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ let app=express();
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-    origin:["http://localhost:5173","http://localhost:5174"],
+    origin:["https://onecart-frontend2-yg0h.onrender.com","http://localhost:5174"],
     credentials:true
 }));
 app.use("/api/auth",authRoute);
