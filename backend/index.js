12bd7b0a40fd5d259d0665fabfcf545afad5f12a
Urbi Mangal <urbimangal@gmail.com>
2026-06-29 16:49:09 +0530

    Update CORS origin for admin frontend

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ let app=express();
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-    origin:["https://onecart-frontend2-yg0h.onrender.com","http://localhost:5174"],
+    origin:["https://onecart-frontend2-yg0h.onrender.com","https://onecart-admin2-0555.onrender.com"],
     credentials:true
 }));
 app.use("/api/auth",authRoute);
