c8f022760c5c82de20ccd0b63507b8a4adf796e1
Urbi Mangal <urbimangal@gmail.com>
2026-06-29 15:50:27 +0530

    Update cookie settings for secure authentication

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -24,8 +24,8 @@ export const registration = async (req, res) => {
     let token = await genToken(user._id);
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "Strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     console.log(req.body);
@@ -54,8 +54,8 @@ export const login = async (req, res) => {
     let token = await genToken(user._id);
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "Strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return res.status(201).json(user);
@@ -83,8 +83,8 @@ export const googleLogin = async (req, res) => {
     let token = await genToken(user._id);
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "Strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     console.log(user);
@@ -105,8 +105,8 @@ export const adminLogin = async (req, res) => {
       let token = await genToken1(email)
       res.cookie("token", token, {
         httpOnly: true,
-        secure: false,
-        sameSite: "Strict",
+        secure: true,
+        sameSite: "none",
         maxAge: 1 * 24 * 60 * 60 * 1000,
       });
       return res.status(200).json(token)
