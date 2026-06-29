c845671f3b07d0a8cafb37f3a3f4f05a4507f420
Urbi Mangal <urbimangal@gmail.com>
2026-06-29 16:55:49 +0530

    Update server URL in AuthContext

--- admin/src/context/AuthContext.jsx
+++ admin/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createContext } from 'react'
 export const AuthDataContext=createContext()
 function AuthContext({children}) {
-    let serverUrl="http://localhost:8000"
+    let serverUrl="https://onecart-backend-8d6y.onrender.com"
     let value={
         serverUrl
     }
